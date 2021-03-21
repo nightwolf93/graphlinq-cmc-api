c5702d44245f41b837b107b81d2602728aa3901f
Nightwolf <nico.style931@gmail.com>
2021-03-21 22:01:05 +0100

    Initial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/total_supply', (req, res) => {
     res.send(500000000);
